c1d06feba289d061928cd67233545d8f1d071eeb
soraiathegirleffect <113796635+soraiathegirleffect@users.noreply.github.com>
2022-09-17 19:36:56 +0100

    Add files via upload

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: somartin <somartin@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/14 19:08:22 by somartin          #+#    #+#             */
+/*   Updated: 2022/09/15 12:30:10 by somartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+/*
+int 	main(void)
+{
+	char	t[] = {"Hello im Soraia\n"};
+	ft_putstr(t);
+
+	return (0);
+}*/
