3787c94fd2aae5696715d43850cac1690148244e
soraiathegirleffect <113796635+soraiathegirleffect@users.noreply.github.com>
2022-09-24 23:49:50 +0100

    Add files via upload

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: somartin <somartin@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/19 20:40:34 by somartin          #+#    #+#             */
+/*   Updated: 2022/09/21 22:31:15 by somartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	sign = 1;
+	result = 0;
+	i = 0;
+	while (str[i] != '\0' && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+		i++;
+	while (str[i] != '\0' && (str[i] == '-' || str[i] == '+'))
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
